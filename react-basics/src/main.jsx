511f30bbfb2cd6cfbcde43606f8c90cc328ceb0d
Kamil Turhan <kmturhan@gmail.com>
2026-03-08 02:41:23 +0300

    feat(react-basics): add conditional bootstrap classes using template literals

--- react-basics/src/main.jsx
+++ react-basics/src/main.jsx
@@ -1,61 +1,73 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './index.css';
 
 function App() {
     return (
       <>
         <Header />
-        <ProductList />
-        <Footer />
+        <div className='container'>
+          <ProductList />
+        </div>
       </>
     );
 }
 
 function Header() {
-  return (<h1>Header</h1>);
+  return (
+    <header>
+      <nav className='navbar navbar-expand bg-dark border-bottom border-body' data-bs-theme="dark">
+        <div className='container'>
+          <a href="#" className='navbar-brand'>Store App</a>
+        </div>
+      </nav>
+    </header>
+  );
 }
 
 function ProductList() {
   const items = [
     {
-      "image": "phone1.webp",
+      "image": "phone2.jpg",
       "title": "Iphone",
       "description": "Lorem ipsum",
-      "price": 80000,
-      "is_active": false
+      "price": 150000,
+      "is_active": true
     },
     {
-      "image": "phone1.webp",
+      "image": "phone2.jpg",
       "title": "Iphone",
       "description": "Lorem ipsum",
       "price": 40000,
-      "is_active": false
+      "is_active": true
     },
     {
-      "image": "phone1.webp",
+      "image": "phone2.jpg",
       "title": "Iphone2",
       "description": "Lorem ipsum",
       "price": 10000,
       "is_active": true
     },
     {
-      "image": "phone1.webp",
+      "image": "phone2.jpg",
       "title": "Iphone3",
       "description": "Lorem ipsum",
       "price": 20000,
-      "is_active": false
+      "is_active": true
     }
-  ]
-  // const items = [];
+  ];
   return (
     <>
-    <h2>Product List</h2>
+    <h2 className='title'>Product List</h2>
     {
       items.length > 0 ? (
-        <div id="product-list">
+        <div className='row row-cols-2 row-cols-md-3 row-cols-cl-4 g-4' id="product-list">
         {
           items.map((item, index) => (
-            <Product key={ index } productObj={ item } />
+            <div className='col'>
+              <Product key={ index } productObj={ item } />
+            </div>
           ))
         }
         </div>   
@@ -71,11 +83,14 @@ function Product({ productObj }) {
   console.log(productObj);
   if(!productObj.is_active) return null;
   return (
-    <div>
-      <img src={ '/img/' + productObj.image }></img>
-      <h2>{ productObj.title }</h2>
-      <p>{ productObj.description }</p>
-      <span>{ productObj.price } TL</span>
+    <div className='card shadow-sm'>
+      <img className='card-img-top p-2 p-md-3 border-bottom' src={ '/img/' + productObj.image }></img>
+      <div className='card-body'>
+        <h2 className='card-title'>{ productObj.title }</h2>
+        <p className='card-text'>{ productObj.description }</p>
+        <span className={ `badge text-bg-success f20 ${productObj.price < 100000 ? "discount" : "price"}`}>{ productObj.price } TL</span>
+      </div>
+      
     </div>
   );
 }
